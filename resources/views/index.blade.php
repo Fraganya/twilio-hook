8d59bacfeed46ff0008afb99514c02a0f917f0fe
Francis Ganya <slimgaxienza@live.com>
2021-05-01 18:30:33 +0200

    removed laravel default landing page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Twilio Hook</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+<div class="text-center">
+    <h1>Twilio Hook</h1>
+    <p>
+        Landing page coming soon.
+    </p>
+</div>
+</body>
+</html>
